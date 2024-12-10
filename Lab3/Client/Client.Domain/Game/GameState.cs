3237e16221ab8dc344ff466889ebeb925992219a
Yark13 <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2024-12-10 12:46:46 +0200

    Added GameStatus and GameState

---
+++ Lab3/Client/Client.Domain/Game/GameState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Domain.Game;
+
+public struct GameState
+{
+    public int[,] Cells;
+    public GameStatus Status;
+
+
+}
---
+++ Lab3/Client/Client.Domain/Services/ServerService/IServerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Domain.Services.ServerService;
+
+public interface IServerService
+{
+     
+}
