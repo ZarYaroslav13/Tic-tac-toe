b08bc29b4b511ff0334c739060028815179a1fd0
Yark13 <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2024-12-10 12:57:49 +0200

    Added ObservableObject

---
+++ Lab3/Client/Client.Domain/Services/ObservableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Domain.Services;
+
+public class ObservableObject : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string property = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+    }
+}
